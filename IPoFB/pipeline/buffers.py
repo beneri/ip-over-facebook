6bb27cd84db6a0350d61f6c9c3a24c251f86433b
Jacotsu <Jacotsu@users.noreply.github.com>
2019-07-06 17:30:13 +0200

    Added FileBinaryBuffer and tests

--- IPoFB/pipeline/buffers.py
+++ IPoFB/pipeline/buffers.py
@@ -11,3 +11,18 @@ class SimpleBinaryBuffer(PipelineBlock):
 
     def send(self, data):
         self._buffer += data
+
+
+class FileBinaryBuffer(PipelineBlock):
+    def __init__(self, filename='buffer', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filename = filename
+        self._buffer = b''
+
+    def recv(self):
+        with open(self.filename, 'rb') as f:
+            return f.read()
+
+    def send(self, data):
+        with open(self.filename, 'wb') as f:
+            f.write(data)
--- tests/unittests/pipeline/test_encoders.py
+++ tests/unittests/pipeline/test_encoders.py
@@ -1,7 +1,8 @@
 import base64
 import pytest
+from os.path import join
 from IPoFB.pipeline.encoders import Base64Encoder
-from IPoFB.pipeline.buffers import SimpleBinaryBuffer
+from IPoFB.pipeline.buffers import SimpleBinaryBuffer, FileBinaryBuffer
 
 
 @pytest.fixture
@@ -21,3 +22,24 @@ class TestBase64Encoder:
         pipeline_buffer._buffer = base64.b64encode(test_data)
         encoder = Base64Encoder(previous_block=pipeline_buffer)
         assert encoder.recv() == test_data
+
+
+class TestFileBuffer:
+    def test_send(self, tmp_path, pipeline_buffer):
+        test_data = b'This is a test string'
+        filename = join(tmp_path, 'buffer')
+        buff = FileBinaryBuffer(filename=filename)
+        buff.send(test_data)
+
+        with open(filename, 'rb') as f:
+            assert f.read() == test_data
+
+    def test_recv(self, tmp_path, pipeline_buffer):
+        test_data = b'This is a test string'
+        filename = join(tmp_path, 'buffer')
+        buff = FileBinaryBuffer(filename=filename)
+
+        with open(filename, 'wb') as f:
+            f.write(test_data)
+
+        assert buff.recv() == test_data
